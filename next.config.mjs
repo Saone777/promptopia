83210a8ea0ef6144ec3530ec6ff0ba40e3fc6437
Sawan Kumar <Saone775071@gmail.com>
2024-07-16 10:10:05 +0530

    fix typo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// Import the required modules if necessary
+// import someModule from 'some-module';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
